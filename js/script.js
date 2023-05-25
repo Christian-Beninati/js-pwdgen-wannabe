63f7956fcc39f4521464c8920f9c92cc270346b7
Christian-Beninati <christianbeninati.info@gmail.com>
2023-05-25 16:36:39 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('JS OK')
+// console.log('JS OK')
+
+// Recuperare il placeholder dal DOM" 
+const passwordPlaceholder = document.getElementById("password-result");
+console.log(passwordPlaceholder);
+
+// Chiedere all'utente il suo nome
+const username = prompt("Inserisci il tuo nome:");
+console.log(username);
+
+// Chiedere all'utente il suo cognome
+const usersurname = prompt("Inserisci il tuo cognome:");
+console.log(usersurname);
+
+// Chiedere all'utente il suo colore preferito
+const favouritecolour = prompt("Inserisci il tuo colore preferito:");
+console.log(favouritecolour);
+
+// Creare la password combinando le informazioni inserite dall'utente
+const password = `${username}${usersurname}${favouritecolour}21`;
+console.log(password);
+
+// Inserire il risultato nell'elemento del DOM
+passwordPlaceholder.innerText = password;
